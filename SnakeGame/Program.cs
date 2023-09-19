b730e7568b4004f1d3b03f2f5fc214feae852e81
Matheus Dias <matheusdiassandes@outlook.com>
2023-09-19 20:05:50 -0300

    adicionando logica metodo CriarCobra e AtualizarPosicaoCobra

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -12,12 +12,74 @@ Direcao direcao = Direcao.Direita;
 int placar = 0;
 Random random = new();
 
-public void IniciarJogo()
+void IniciarJogo()
 {
+    CriarCobra();
+    CriarComida();
+    LerTeclas();
 
+    while (jogoRodando)
+    {
+        Thread.Sleep(50);
+        TransladarCobra();
+        Renderizar();
+    }
+
+    FimDeJogo();
 }
 
+void FimDeJogo()
+{
+    throw new NotImplementedException();
+}
 
+void Renderizar()
+{
+    throw new NotImplementedException();
+}
 
+void TransladarCobra()
+{
+    throw new NotImplementedException();
+}
 
+void LerTeclas()
+{
+    throw new NotImplementedException();
+}
 
+void CriarComida()
+{
+    throw new NotImplementedException();
+}
+
+void CriarCobra()
+{
+    coordenadasCobra.Add(new Coordenada(7, 14));
+    coordenadasCobra.Add(new Coordenada(8, 14));
+    coordenadasCobra.Add(new Coordenada(9, 14));
+
+    AtualizarPosicaoCobra();
+}
+
+void AtualizarPosicaoCobra()
+{
+    for(int l = 0; l < larguraTela; l++)
+    {
+        for(int a = 0; a < larguraTela; a++)
+        {
+            var posicaoDeveConterCobra = coordenadasCobra.Any(coordenada => coordenada.X == l && coordenada.Y == a);
+
+            if(posicaoDeveConterCobra)
+            {
+                tela[l, a] = caractereCobra;
+                continue;
+            }
+
+            if (tela[l,a] == caractereCobra)
+            {
+                tela[l, a] = " ";
+            }
+        }
+    }
+}
