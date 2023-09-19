07719b6f5e8c4e895fff6d3703d29d712aea743a
Matheus Dias <matheusdiassandes@outlook.com>
2023-09-19 19:48:48 -0300

    adicionando variaveis, listas e objetos

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using SnakeGame;
+
+const int larguraTela = 69;
+const int alturaTela = 29;
+const string caractereCobra = "■";
+
+string[,] tela = new string[larguraTela, alturaTela];
+bool jogoRodando = true;
+List<Coordenada> coordenadasCobra = new();
+
+Direcao direcao = Direcao.Direita;
+int placar = 0;
+Random random = new();
